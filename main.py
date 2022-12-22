45eda7285140abf4675d387dec2da6ae750d3398
Haniyeh2000 <59335965+Haniyeh2000@users.noreply.github.com>
2022-12-23 00:57:07 +0330

    adding my name

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # This is a sample Python script.
 
+
+# my name is hanieh yazdian
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
